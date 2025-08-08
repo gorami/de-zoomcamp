b2bd6726e9cd39b580d4b589681934dc7cafd7aa
gora sudindranath <gorami@gmail.com>
2025-08-08 20:37:42 +0000

    added quarter to query

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -48,7 +48,8 @@ select trips_unioned.tripid,
     trips_unioned.improvement_surcharge, 
     trips_unioned.total_amount, 
     trips_unioned.payment_type, 
-    trips_unioned.payment_type_description
+    trips_unioned.payment_type_description,
+    extract(quarter from trips_unioned.pickup_datetime) as trip_quarter
 from trips_unioned
 inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid = pickup_zone.locationid
