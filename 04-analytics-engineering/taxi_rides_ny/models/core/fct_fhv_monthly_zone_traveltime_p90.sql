5ff73a9f515dfd5f1b962d9619ded29028caf2e2
gora sudindranath <gorami@gmail.com>
2025-08-19 14:41:46 +0000

    compute p90 of trip duration

---
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -0,0 +1,11 @@
+with trips_data as (select * from `prod.dim_fhv_trips`)
+select distinct
+    trip_month,
+    trip_year,
+    pickup_locationid,
+    dropoff_locationid,
+    percentile_cont(trip_duration, 0.90) over (
+        partition by trip_month, trip_year, pickup_locationid, dropoff_locationid
+    ) p90
+from trips_data
+;
